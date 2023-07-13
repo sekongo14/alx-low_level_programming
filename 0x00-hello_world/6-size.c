df8dcc5e02aac73d544a6434e11863405943b049
sekongo <sekongoismael0@gmail.com>
2023-07-13 14:16:44 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: alwas 0 (succes)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char);
+	printf("Size of an int: %lu byte(s)", sizeof(int);
+	printf("Size of a long int: %lu byte(s)", sizeof(long int);
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int);
+	printf("Size of a float: %lu byte(s)", sizeof(float);
+	return (0);
+
+}
