475165ddafc660a0894390fdbc0ad4133bdb2297
sekongo <sekongoismael0@gmail.com>
2023-07-13 11:02:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return: alwas 0 (succes)
+ * Return: alwas 0 (succes)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
