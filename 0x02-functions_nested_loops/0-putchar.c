d5596c502f6beb63178f6565d3283f8d47c61c33
sekongo <sekongoismael0@gmail.com>
2023-07-18 10:46:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - entry point
+ * Descrition print _putchar
+ * Return: 0 always
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0)
+}
