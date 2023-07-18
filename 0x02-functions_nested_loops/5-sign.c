85c91689c0913d282df7ba601256b58390229ead
sekongo <sekongoismael0@gmail.com>
2023-07-18 15:28:28 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - entry point
+ * @n: variable print
+ * Return: 0 always
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
