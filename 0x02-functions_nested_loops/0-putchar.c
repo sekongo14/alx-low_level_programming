0f0397b97dad90861528c22a209f41a09094029b
sekongo <sekongoismael0@gmail.com>
2023-07-18 09:48:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+/* more headers goes there */
+/* betty style doc for function main goes there*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
