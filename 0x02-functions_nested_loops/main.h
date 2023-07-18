69c3fcaaa45bca9d5d8d7fe82c6e94e21d8156c8
sekongo <sekongoismael0@gmail.com>
2023-07-18 16:02:47 -0700

    mise a jour de main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 
 #endif
