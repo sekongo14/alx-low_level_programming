5f3354cb67cc88cc81841609116817bade2e26ca
sekongo <sekongoismael0@gmail.com>
2023-07-18 18:57:22 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,24 +6,36 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			printf("%d\n", n);
-		}
-	}
-	else if (n > 98)
-	{
-		for (i = n; i = 98; i--)
-		{
-			printf("%d\n", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+			}
 		}
 	}
 	else
 	{
-		printf("%d\n", n);
+		for (; n > 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
 	}
 }
