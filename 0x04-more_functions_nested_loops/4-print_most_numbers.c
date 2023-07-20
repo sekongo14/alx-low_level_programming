a83c6055e9688db352de1509780a6215e4480f36
sekongo <sekongoismael0@gmail.com>
2023-07-20 11:31:32 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Return: 1 if c is upper  else return 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;
+		}
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
