d48b7d5102da76a5465072b94e666fbb8c725263
sekongo <sekongoismael0@gmail.com>
2023-07-20 11:01:20 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<ctype.h>
+/**
+ * _isupper - entry point
+ * @c: variable
+ * Return: 1 if c is upper  else return 0
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
