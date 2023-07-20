8eb4f267e8bbc2d727636b854c0ba021ccade128
sekongo <sekongoismael0@gmail.com>
2023-07-20 15:10:15 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,10 @@ void print_line(int n)
 {
 	int i;
 
-	if (n == 0 || n < 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-	}
+	
 	_putchar('\n');
 }
