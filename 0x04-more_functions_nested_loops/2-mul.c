665daffb98ba014ab4395ac37f68bff2aa21e0d8
sekongo <sekongoismael0@gmail.com>
2023-07-20 11:20:56 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<ctype.h>
+/**
+ * mul - entry point
+ * @a: variable1
+ * @b: variable2
+ * Return: 1 if c is upper  else return 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
