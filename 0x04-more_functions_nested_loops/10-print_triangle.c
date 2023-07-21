a673dfe6c7c6b3355d0bdea4566cd6ca431c3039
sekongo <sekongoismael0@gmail.com>
2023-07-20 18:26:55 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * @size: variable
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+		if(size <= 0)
+		{
+			_putchar('\n');
+		}
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (int k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
