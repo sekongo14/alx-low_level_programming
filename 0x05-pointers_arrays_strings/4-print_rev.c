706b39966acdeaea79c40bda8de0c782d48a94a7
sekongo <sekongoismael0@gmail.com>
2023-07-24 11:07:42 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * @s: pointer
+ */
+void print_rev(char *s)
+{
+	int leng;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+	s--;
+	while (leng > 0)
+	{
+		_putchar(*s);
+		s--;
+		leng--;
+	}
+}
