f5fe0a7f3cd4d9c8bc2efdb92251c03a5e398fca
sekongo <sekongoismael0@gmail.com>
2023-07-24 14:27:55 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/**
+ * rev_string - entry point
+ *@s: pointer
+ */
+void rev_string(char *s)
+{
+	int leng = 0;
+	int debut = 0;
+	int fin = leng - 1;
+	int temp;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+	while (debut < fin)
+	{
+		temp = s[debut];
+		s[debut] = s[fin];
+		s[fin] = temp;
+		debut++;
+		fin--;
+	}
+
+
+}
