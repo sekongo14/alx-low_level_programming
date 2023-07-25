88a91d4e4685937c0eeef47cb6e63e0c676464b7
sekongo <sekongoismael0@gmail.com>
2023-07-24 23:36:14 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+void puts_half(char *str)
+{
+	leng = 0;
+	leng1;
+
+	while (str[leng] != '\0')
+	{
+		leng++;
+	}
+	leng1 = leng / 2;
+
+	int i;
+
+	for (i = leng1; i < leng; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
