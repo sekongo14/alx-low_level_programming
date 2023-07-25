ae42c529a4c2573bb39113ae38aa29e41073345f
sekongo <sekongoismael0@gmail.com>
2023-07-25 14:30:16 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_array - entry point
+ * @a: pointer
+ * @n: variable
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *a);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+}
