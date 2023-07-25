5539f6ef73a9cbe451f62f21d01a83897c63bb7b
sekongo <sekongoismael0@gmail.com>
2023-07-25 15:53:16 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - entry point
+ * @dest: pointer 1
+ * @src: pointer source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
