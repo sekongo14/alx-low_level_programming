71b073cdc42e11a510e3b01fcf2862126048e63c
sekongo <sekongoismael0@gmail.com>
2023-07-26 13:48:10 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include  "main.h"
+/**
+ * _strncat - entry point
+ * @dest: pointer 1
+ * @src: pointer source
+ * @n: variable
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int leng = 0;
+	int i = 0;
+
+	while (dest[leng] != '\0')
+	{
+		leng++;
+	}
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[leng + i] = src[i];
+		i++;
+	}
+	dest[dest_length + i] = '\0';
+	return (dest);
+}
