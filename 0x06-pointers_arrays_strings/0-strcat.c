3f24a60bfe87dc52d4410109e5fa445c84a273be
sekongo <sekongoismael0@gmail.com>
2023-07-26 10:13:46 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - entry point
+ * @dest: pointer 1
+ * @src: pointer 2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int leng =0;
+	int j = 0;
+
+	while (dest[leng] != '\0')
+	{
+		leng ++;
+	}
+
+	while(src[j] != '\0')
+	{
+		dest[leng] = src[j];
+		leng++;
+		j++;
+	}
+
+	dest[leng] = '\0';
+	return dest;
+}
