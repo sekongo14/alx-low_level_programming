f807ed357ccecc87329c1f1e3f94bdb5f80288c4
sekongo <sekongoismael0@gmail.com>
2023-07-31 10:11:19 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strchr - entry point
  * @s: pointer
@@ -7,13 +8,5 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	return (NULL);
+	return (strchar(s, c));
 }
