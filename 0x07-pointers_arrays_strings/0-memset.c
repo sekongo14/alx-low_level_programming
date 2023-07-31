293162ce153eb0eaf70f2aed8363e62bc63658f9
sekongo <sekongoismael0@gmail.com>
2023-07-31 07:41:09 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - entry point
+ * @s: pointer
+ * @b: variable
+ * @n: unsigned var
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (0);
+}
