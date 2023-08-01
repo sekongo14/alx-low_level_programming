471969b5dfeb3f82fa9418cebbe7e8217a14d1aa
sekongo <sekongoismael0@gmail.com>
2023-07-31 23:18:52 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - function name
+ * @a: pointer
+ * @size: varisble
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sommeDiagonalePrincipale = 0;
+	int sommeDiagonaleSecondaire = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sommeDiagonalePrincipale += a[i * size + i];
+		sommeDiagonaleSecondaire += a[i * size + (size - 1 - i)];
+	}
+	printf("%d",sommeDiagonalePrincipale);
+	printf(", ");
+	printf("%d",sommeDiagonaleSecondaire);
+	printf("\n");
+}
