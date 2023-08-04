f59b03fa8b4e4540885afcecfad64f97ff9f6722
sekongo <sekongoismael0@gmail.com>
2023-08-04 16:01:07 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf"%s\n", (argv[0]);
+
+	return (0);
+}
+
