44ecdc9f60c938f551f6b6b4e75fcc7f3e073b3c
sekongo <sekongoismael0@gmail.com>
2023-08-04 16:34:11 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
