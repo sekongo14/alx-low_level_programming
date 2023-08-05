b0574c7132f6bffdea16ed9c64788e55baf60d0c
sekongo <sekongoismael0@gmail.com>
2023-08-04 22:49:42 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,42 @@
-#include "main.h"
 #include <stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
 
 /**
- * main - entry point
- * @argc: size of argv
- * @argv: list
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, sum, j;
 
 	if (argc == 1)
 	{
-		printf("0\n");
+		printf("%d\n", 0);
+		return (0);
 	}
-	else if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			int j;
-			int len = strlen(argv[i]);
 
-			for (j = 0; j < len; j++)
+	sum = 0;
+
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
 			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[i]);
+			j++;
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+
+	printf("%d\n", sum);
 	return (0);
 }
