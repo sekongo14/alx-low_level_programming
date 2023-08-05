b120b92ed775fb02a5d25bfcfe41019a9e45190c
sekongo <sekongoismael0@gmail.com>
2023-08-04 17:15:53 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc == 1)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
