de93afbc1bbafcc316a6dc19bc3b1a4d72036304
sekongo <sekongoismael0@gmail.com>
2023-08-08 14:38:42 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(height * sizeof(int));
+	grid =(int **) malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
 		return (NULL);
