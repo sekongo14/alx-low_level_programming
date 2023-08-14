7436c912fc5922f9dd26b82645c31caad1c46814
sekongo <sekongoismael0@gmail.com>
2023-08-14 09:53:58 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		d = malloc(sizeof(struct dog));
-	}
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
