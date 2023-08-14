a13b1b265e986e3782da7c0eeb74a6258aeb8d40
sekongo <sekongoismael0@gmail.com>
2023-08-14 04:20:27 -0700

    dog.h

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - structure dog
+ * @name: the name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: not description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
