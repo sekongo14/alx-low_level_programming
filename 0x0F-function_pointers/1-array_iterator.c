b201d58e55112df7ea413e10e4f0c4f77ee2e14a
sekongo <sekongoismael0@gmail.com>
2023-08-16 07:49:50 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * array_iterator - fonction principal
+ * @array: pointer array
+ * @size: size of array
+ * @action: fonction 2
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
