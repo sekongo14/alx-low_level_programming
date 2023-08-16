e9533a0aa50c9c5824314406c50a2f402c9329ff
sekongo <sekongoismael0@gmail.com>
2023-08-16 09:43:20 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-cal.h"
+
+/**
+ * op_add - add function
+ * @a: variable1
+ * @b:variable2
+ * Return: add
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub function
+ * @a: variable1
+ * @b:variable2
+ * Return: sub of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multip
+ * @a: variable1
+ * @b:variable2
+ * Return: multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divise
+ * @a: variable1
+ * @b:variable2
+ * Return: multiplication of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - function op_mod
+ * @a: variable1
+ * @b:variable2
+ * Return: multiplication of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
