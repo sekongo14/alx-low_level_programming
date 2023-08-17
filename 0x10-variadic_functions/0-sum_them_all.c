e192887d3c10fbffaeaf89f83177562e1e7ad4c9
sekongo <sekongoismael0@gmail.com>
2023-08-17 05:56:57 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-                return (0);
 
 	unsigned int i;
 	int sum = 0;
-
 	va_list op;
+
+	if (n == 0)
+		return (0);
 
 	va_start(op, n);
 
