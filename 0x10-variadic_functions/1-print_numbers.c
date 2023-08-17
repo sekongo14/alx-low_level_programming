57701cb6ff976f036270a85771cffa1528f41824
sekongo <sekongoismael0@gmail.com>
2023-08-17 07:51:13 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 /**
- * sum_them_all - function sum
+ * print_numbers - function prints number
  * @n: variable1
- * Return: sum
+ * @separator: separator
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -19,11 +19,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(op, int));
-		if(separator == NULL)
+		if (separator == NULL)
 		{
 			continue;
 		}
-		if(i < n - 1)
+		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
