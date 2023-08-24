0ea54bf9e90d8050ce64826080de395c9e6e1b96
sekongo <sekongoismael0@gmail.com>
2023-08-24 05:44:04 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t val;
 
-	i = 0;
+	val = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		val++;
 	}
-	return (i);
+	return (val);
 }
