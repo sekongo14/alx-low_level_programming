7118c13c5952364cd86a628bb1c1c3d5a59b63c8
sekongo <sekongoismael0@gmail.com>
2023-08-25 02:17:02 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -36,13 +36,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = new_node;
-	}
 	else
 	{
 		list_t *current;
+
 		current = *head;
+
 		while (current->next != NULL)
 		{
 			current = current->next;
