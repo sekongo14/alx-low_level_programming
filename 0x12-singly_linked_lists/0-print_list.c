fde8731f8a80a051157966127a41cf9db454790a
sekongo <sekongoismael0@gmail.com>
2023-08-25 01:48:27 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	val = 0;
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%lu] %s\n", (unsigned long)h->len, h->str);
