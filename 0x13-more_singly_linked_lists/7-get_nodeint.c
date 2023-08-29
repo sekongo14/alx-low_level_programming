1164a1f5a5b0cdae2dd1f704f5ec73d15565181b
sekongo <sekongoismael0@gmail.com>
2023-08-29 10:54:53 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
  * get_nodeint_at_index - pops a list element from the beginning of a list
  * @head: double pointer to head of list
  * @index: index of number
- * Return: 
+ * Return: Null
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
