075173fb5c5879b71c4a4ef7e96d47cd36868eb6
sekongo <sekongoismael0@gmail.com>
2023-08-29 07:20:34 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t **tmp;
 
 	while (head)
 	{
-		tmp = *head;
-		*head = *head->next;
+		*tmp = *head;
+		head = head->next;
 		free(tmp);
 	}
 }
