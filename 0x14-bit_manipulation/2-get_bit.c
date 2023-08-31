2a7eef08f96c2142b521e89464a3a85595ed31b0
sekongo <sekongoismael0@gmail.com>
2023-08-31 15:23:29 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8) /* Ensure index is within valid range */
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	return ((n >> index) & 1);
