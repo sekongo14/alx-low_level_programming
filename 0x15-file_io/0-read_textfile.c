a159713d8bd1042cd5a1302463c09c9bb8234afb
sekongo <sekongoismael0@gmail.com>
2023-09-07 03:05:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,18 +6,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE fichier;
+	FILE *fichier;
+	char *ligne;
 
-	fichier = fopen(filename, "r")
+	fichier = fopen(filename, "r");
 	if (filename == NULL)
 	{
 		return (0);
 	}
 
-	while (fgets(letters, sizeof(letters), fichier) != NULL)
+	ligne = malloc(sizeof(letters));
+	if (ligne == NULL)
 	{
-		printf("%s", letters);
+		return (0);
 	}
+
+	while (fgets(ligne, letters, fichier) != NULL)
+	{
+		printf("%s", ligne);
+	}
+	free(ligne);
 
 	fclose(fichier);
 	return (0);
